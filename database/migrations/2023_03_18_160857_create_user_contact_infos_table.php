40eed1d72a6696341a35232fabc693b2ba658b4d
SretenJovanovic <sreten93jovanovic@gmail.com>
2023-03-18 20:49:11 +0100

    User,Profile,Role migrations

--- database/migrations/2023_03_18_160857_create_user_contact_infos_table.php
+++ database/migrations/2023_03_18_160857_create_user_contact_infos_table.php
@@ -11,13 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_contact_infos', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('adress');
+            $table->string('city');
+            $table->string('phone');
+
+            $table->foreignId('profile_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
+            
         });
     }
 
@@ -26,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_contact_infos');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,52 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+        ]);
+        \App\Models\User::factory()->create([
+            'email' => 'tehnicar@tehnicar.com',
+            'password' => Hash::make('tehnicar'),
+        ]);
+        \App\Models\User::factory()->create([
+            'email' => 'operator@operator.com',
+            'password' => Hash::make('operator'),
+        ]);
+
+        \App\Models\Role::factory()->create([
+            'type' => 'admin',
+        ]);
+        \App\Models\Role::factory()->create([
+            'type' => 'operator',
+        ]);
+        \App\Models\Role::factory()->create([
+            'type' => 'technician',
+        ]);
+
+        \App\Models\Profile::factory()->create([
+            'username' => 'Test',
+            'first_name' => 'Test',
+            'last_name' => 'Test',
+            'age' => 25,
+            'user_id' => 1,
+            'role_id' => 1,
+        ]);
+        \App\Models\Profile::factory()->create([
+            'username' => 'Test',
+            'first_name' => 'Test',
+            'last_name' => 'Test',
+            'age' => 25,
+            'user_id' => 2,
+            'role_id' => 3,
+        ]);
+        \App\Models\Profile::factory()->create([
+            'username' => 'Test',
+            'first_name' => 'Test',
+            'last_name' => 'Test',
+            'age' => 25,
+            'user_id' => 3,
+            'role_id' => 2,
+        ]);
     }
 }
